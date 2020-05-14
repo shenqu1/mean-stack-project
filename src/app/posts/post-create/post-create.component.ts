9cdbf853ad666359d7422b7fa08718c1afd959d6
Shen Qu <qushen024@hotmail.com>
2020-05-13 20:58:23 -0400

    Angular Frontend Structure

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+  enteredTitle = '';
+  enteredContent = '';
+
+  constructor(public postsService: PostsService) { }
+
+  ngOnInit(): void {
+  }
+
+    onAddPost(form: NgForm) {
+      if(form.invalid) {
+        return;
+      }
+      this.postsService.addPost(form.value.title, form.value.content);
+      form.resetForm();
+    }
+}
