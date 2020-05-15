6bd8289b7f0164d79b8f0d5ee8d772e2f6923a76
Shen Qu <qushen024@hotmail.com>
2020-05-15 18:09:58 -0400

    Navigate user and add loading spinners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -33,7 +34,8 @@ import { AppRoutingModule } from './app-routing.modules';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
-    HttpClientModule
+    HttpClientModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,13 +3,14 @@ import { Subject } from 'rxjs';
 import { Post } from './post.model';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
     private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>();
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private router: Router) {}
 
     getPosts() {
         this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
@@ -44,6 +45,7 @@ export class PostsService {
                 post.id = id;
                 this.posts.push(post);
                 this.postsUpdated.next([...this.posts]);
+                this.router.navigate(["/"]);
             });
         
     }
@@ -59,6 +61,7 @@ export class PostsService {
                 updatedPosts[oldPostIndex] = post;
                 this.posts = updatedPosts;
                 this.postsUpdated.next([...this.posts]);
+                this.router.navigate(["/"]);
             });
     }
 
